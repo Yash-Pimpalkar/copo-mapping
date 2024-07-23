4d4034de024026dce8c07c3b8974c6788d6fffbd
Yash Pimpalkar <138708785+Yash-Pimpalkar@users.noreply.github.com>
2024-07-23 12:35:39 +0530

    Delete Day 1 directory

--- Day 1/Backend/routes/register.js
+++
@@ -1,9 +0,0 @@
-import express, { Router } from "express";
-const router =express.Router();
-import { getemail, getusers, register, setusertype } from "../controller/register.js"
-
-router.post("/",register);
-router.post('/getemail/:id',getemail);
-router.post('/getusers',getusers);
-router.post('/updateusertype',setusertype);
-export default router;
