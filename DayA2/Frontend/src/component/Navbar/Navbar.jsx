a46199491080b43bcd2d6df2e6d24f7b712ff26b
Nihaar75858 <nihar.adhiep@gmail.com>
2024-07-23 16:28:08 +0530

    second

--- DayA2/Frontend/src/component/Navbar/Navbar.jsx
+++ DayA2/Frontend/src/component/Navbar/Navbar.jsx
@@ -118,7 +118,7 @@ export default function Navbar() {
   const formNavigation = [
     { name: 'Add CO Form', href: '/coform', userTypes: [2] },
     { name: 'Add PO Form', href: '/posform', userTypes: [2] },
-    { name: 'Add Course', href: '/course', userTypes: [2] }
+    { name: 'Add Course', href: '/courseform', userTypes: [2] }
   ];
 
   const formCurriculum = [
