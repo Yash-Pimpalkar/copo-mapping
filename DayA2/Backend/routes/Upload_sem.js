38ebca2bc8924e7872b062fb29ab7727134e6b74
Shantanu Vedante <vu1f2122110@pvppcoe.ac.in>
2024-08-30 15:17:08 +0530

    Update Upload_sem.js

--- DayA2/Backend/routes/Upload_sem.js
+++ DayA2/Backend/routes/Upload_sem.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { SemUpload, showSemData, upload_Sem_Questions } from "../controller/Upload_sem.js";
+import { limit, SemUpload, showSemData, upload_Sem_Questions } from "../controller/Upload_sem.js";
 const router =express.Router();
 
 
 router.post("/create",upload_Sem_Questions);
 router.get("/show/:uid",showSemData)
 router.put("/",SemUpload);
+router.get("/limit/:uid",limit);
 export default router;
