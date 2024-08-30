56a98742d4a3ef3db220e8cb0816838877316340
Yash Pimpalkar <138708785+Yash-Pimpalkar@users.noreply.github.com>
2024-08-30 19:00:24 +0530

    Update user_course.js

--- DayA2/Backend/routes/user_course.js
+++ DayA2/Backend/routes/user_course.js
@@ -1,9 +1,10 @@
 import express, { Router } from "express";
-import { show_CoCount, show_user_course, user_course_registration } from "../controller/user_course.js";
+import { coname, show_CoCount, show_user_course, user_course_registration } from "../controller/user_course.js";
 const router =express.Router();
 
 
 router.post("/",user_course_registration);
 router.post("/:uid",show_user_course);
 router.get("/cocount/:uid",show_CoCount);
+router.get("/coname/:uid",coname);
 export default router;
