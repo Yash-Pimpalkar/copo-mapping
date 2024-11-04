89976e6b5fac29e73d2a3047406a22408211fefe
Yash Pimpalkar <138708785+Yash-Pimpalkar@users.noreply.github.com>
2024-11-05 03:41:37 +0530

    Update feedback.js

--- DayA2/Backend/routes/lms/feedback.js
+++ DayA2/Backend/routes/lms/feedback.js
@@ -4,6 +4,8 @@ import {
   show_feedback,
   student_submit_feedback,
   show_student_side_feedback,
+  getStudentFeedbackDetails,
+  updateFeedbackAttainment,
 } from "../../controller/lms/feedback.js";
 
 const router = express.Router();
@@ -12,5 +14,6 @@ router.post("/create", create_feedback);
 router.get("/show/teacher/:uid", show_feedback);
 router.post("/submit/:sid/:uid", student_submit_feedback);
 router.get("/show/student/:sid", show_student_side_feedback);
-
+router.get("/attainment/teacher/:usercourseid",  getStudentFeedbackDetails);
+router.post("/attainment/update",updateFeedbackAttainment)
 export default router;
